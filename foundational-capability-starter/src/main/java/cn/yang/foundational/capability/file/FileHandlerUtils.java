0d66380146f5d1eecccbfc151345d7e0932e3e90
neverisland <59460062+neverisland@users.noreply.github.com>
2025-11-22 23:50:21 +0800

    feat: 新增了文件上传

---
+++ foundational-capability-starter/src/main/java/cn/yang/foundational/capability/file/FileHandlerUtils.java
@@ -0,0 +1,112 @@
+package cn.yang.foundational.capability.file;
+
+import cn.yang.foundational.capability.file.exception.FileHandlerException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 文件处理工具类
+ *
+ * @author : 未见清海
+ */
+public class FileHandlerUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileHandlerUtils.class);
+
+    // 定义格式
+    static SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+
+    /**
+     * 获取文件保存相对路径
+     *
+     * @param fileName 文件名称
+     * @return 文件保存相对路径
+     */
+    public static String getPath(String fileName) {
+        // 获取当前时间
+        String formattedDate = sdf.format(new Date());
+        return File.separator + formattedDate + File.separator + fileName;
+    }
+
+    /**
+     * 保存文件至本地
+     *
+     * @param fileUrl  文件网络路径
+     * @param savePath 文件本地保存路径
+     * @throws FileHandlerException 文件处理异常
+     */
+    public static void downloadFile(String fileUrl, String savePath) throws FileHandlerException {
+        try {
+            // 创建URL对象
+            URL url = new URL(fileUrl);
+            HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+            httpConn.setRequestMethod("GET");
+
+            int responseCode = httpConn.getResponseCode();
+
+            // 检查响应码是否是200 OK
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                // 新建文件夹
+                mkdirFolder(savePath);
+                // 读取输入流
+                InputStream inputStream = httpConn.getInputStream();
+                // 创建文件输出流
+                FileOutputStream outputStream = new FileOutputStream(savePath);
+
+                int bytesRead;
+                byte[] buffer = new byte[4096];
+
+                // 写入文件
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+
+                // 关闭流
+                outputStream.close();
+                inputStream.close();
+
+                logger.info("文件已下载并保存至:{}", savePath);
+            } else {
+                logger.warn("无法连接到文件 URL，响应码:{}", responseCode);
+                throw new FileHandlerException("无法连接到文件 URL，响应码" + responseCode);
+            }
+            httpConn.disconnect();
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+            throw new FileHandlerException("文件处理异常..");
+        }
+    }
+
+    /**
+     * 创建上一级别的文件夹
+     *
+     * @param path 文件路径
+     */
+    private static void mkdirFolder(String path) throws FileHandlerException {
+        Path filePath = Paths.get(path);
+        // 获取父目录
+        Path parentDir = filePath.getParent();
+
+        if (parentDir != null) {
+            try {
+                // 创建父目录及其所有不存在的上级目录
+                Files.createDirectories(parentDir);
+            } catch (IOException e) {
+                logger.error("创建上一级的文件夹失败", e);
+                throw new FileHandlerException("创建上一级的文件夹失败");
+            }
+        }
+    }
+}
---
+++ foundational-capability-starter/src/main/java/cn/yang/foundational/capability/file/exception/FileHandlerException.java
@@ -0,0 +1,44 @@
+package cn.yang.foundational.capability.file.exception;
+
+import java.io.Serial;
+
+/**
+ * 文件处理异常
+ *
+ * @author : 未见清海
+ */
+public class FileHandlerException extends Exception {
+
+    @Serial
+    private static final long serialVersionUID = -7810398818059873465L;
+
+    /**
+     * Constructs a new runtime exception with the specified detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public FileHandlerException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new runtime exception with the specified detail message and
+     * cause.  <p>Note that the detail message associated with
+     * {@code cause} is <i>not</i> automatically incorporated in
+     * this runtime exception's detail message.
+     *
+     * @param message the detail message (which is saved for later retrieval
+     *                by the {@link #getMessage()} method).
+     * @param cause   the cause (which is saved for later retrieval by the
+     *                {@link #getCause()} method).  (A {@code null} value is
+     *                permitted, and indicates that the cause is nonexistent or
+     *                unknown.)
+     * @since 1.4
+     */
+    public FileHandlerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
